f202c81bb20efcc9c1c52daaff004662035005b1
Ahmed <ahmedberriche1993@gmail.com>
2018-12-11 16:50:12 +0100

    Add expensify app Styles

--- src/app.js
+++ src/app.js
@@ -10,17 +10,14 @@ import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 import 'react-dates/lib/css/_datepicker.css';
 import { firebase } from './firebase/firebase';
+import LoadingPage from './components/LoadingPage';
 
 const store = configureStore();
-
 const jsx = (
   <Provider store={store}>
     <AppRouter />
   </Provider>
 );
-
-ReactDOM.render(<p>Loading...</p>, document.getElementById('app'));
-
 let hasRendered = false;
 const renderApp = () => {
   if (!hasRendered) {
@@ -29,7 +26,7 @@ const renderApp = () => {
   }
 };
 
-ReactDOM.render(<p>Loading...</p>, document.getElementById('app'));
+ReactDOM.render(<LoadingPage />, document.getElementById('app'));
 
 firebase.auth().onAuthStateChanged((user) => {
   if (user) {
@@ -46,5 +43,3 @@ firebase.auth().onAuthStateChanged((user) => {
     history.push('/');
   }
 });
-
-
--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,19 +1,18 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import {Link} from 'react-router-dom';
 import moment from 'moment';
 import numeral from 'numeral';
 
-const ExpenseListItem = ({ id, description, amount, createdAt }) => (
-  <div>
-    <Link to={`/edit/${id}`}>
-      <h3>{description}</h3>
+const ExpenseListItem = ({id, description, amount, createdAt}) => (
+    <Link className="list-item" to={`/edit/${id}`}>
+        <div>
+            <h3>{description}</h3>
+            <span>{moment(createdAt).format('MMMM Do, YYYY')}</span>
+        </div>
+        <span>{numeral(amount / 100).format('$0,0.00')}</span>
+
     </Link>
-    <p>
-      {numeral(amount / 100).format('$0,0.00')}
-      -
-      {moment(createdAt).format('MMMM Do, YYYY')}
-    </p>
-  </div>
+
 );
 
 export default ExpenseListItem;
